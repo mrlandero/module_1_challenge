2435787caae9304788668ccf9d32305f889c53c0
mrlandero <mr.landero@gmail.com>
2021-09-18 12:46:27 -0500

    First Stopping Point Commit

--- module_1_challenge/main.py
+++ module_1_challenge/main.py
@@ -1,4 +1,16 @@
-def hello():
-  print("My name is Tony L.")
 
-hello()
+# List of loan costs stored into variable (loan_costs)
+loan_costs = [500, 600, 200, 1000, 450]
+
+# How many total loans in the list --> Using the len() function
+number_of_loans = len(loan_costs)
+print(f"There are a total of {number_of_loans} loans.")
+
+# Total $ amount of loans in the list --> Using the sum() function
+total_loan_amount = sum(loan_costs)
+print(f"The total cost of the loans is ${total_loan_amount}")
+
+# Average loan price --> total_loan_amount / number_of_loans
+average_loan_price = total_loan_amount / number_of_loans
+print(f"The average price per loan is ${average_loan_price}")
+
